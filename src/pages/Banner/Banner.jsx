7c5b3833b57d99b0ff38c8a6fca8aa43f6fcefe7
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-29 11:22:23 +0600

    banner complete basic structure

--- src/pages/Banner/Banner.jsx
+++ src/pages/Banner/Banner.jsx
@@ -1,70 +1,39 @@
 import React from "react";
 
-import mainImg from '../../assets/image 2.png'
-import woodImg from '../../assets/wood-image.png'
-import SectionTitle from "../SectionTitle/SectionTitle";
+import mainImg from '../../assets/image 2.png';
+import woodImg from '../../assets/wood-image.png';
 
 const Banner = () => {
-    return (
-        <div className="relative w-full bg-PrimaryBg h-screen">
-
-            <div className="container px-20 mx-auto">
-                <div className="grid grid-cols-7">
-                    <div className="col-span-2 border h-96 border-black">
-                        <img className="w-96 origin-center -rotate-45" src={woodImg} alt="" />
-
-                    </div>
-                    <div className="flex items-center justify-center col-span-3 border h-96 border-black ">
-                        <img className="w-[496px] h-[465px]" src={mainImg} alt="" />
-                    </div>
-                    <div className="col-span-2 border h-96 border-black">
-
-                    </div>
-                </div>
-
-                <SectionTitle title={"Banner SEction"}></SectionTitle>
+  return (
+    <div className=''>
+      <div className='bg-PrimaryBg '>
+        <div className='relative container mx-auto pt-10 pb-32 border-4'>
+          <div className="">
+            <p className="absolute top-56 left-0 text-2xl rotate-90 font-semibold">2024  EDITION</p>
+            <div className="">
+              <h3 className="text-[192px] text-[#FFC39A] font-MochiyPopOne absolute top-16 left-[220px] z-20">New</h3>
+              <img className="absolute top-60 -left-16 -rotate-45 " src={woodImg} alt="" />
+              <div className="border-l-4 border-black pl-4 py-2 absolute left-96 bottom-36">
+                <p className="text-sm font-MochiyPopOne ">TimberTrove</p>
+                <p className="text-sm font-MochiyPopOne ">Furniture</p>
+              </div>
             </div>
+            <div className="">
+              <img className="mx-auto z-50" src={mainImg} alt="" />
+              <h3 className="text-[131px] text-[#FF6F00] font-MochiyPopOne absolute bottom-32 right-56">arrivals</h3>
+            </div>
+            <div className="">
 
-
-            {/* <div className="max-w-6xl mx-auto  p-8 relative ">
-        <div className="absolute text-gray-500 left-[-2rem] top-1/2 transform -translate-y-1/2 rotate-[-90deg]">
-          <p className="font-bold tracking-wide">2024 EDITION</p>
-        </div>
-
-        <div className="absolute text-gray-500 right-[-2rem] top-1/2 transform -translate-y-1/2 rotate-[90deg]">
-          <p className="font-bold tracking-wide">2024</p>
-        </div>
-
-        <div className="flex flex-col lg:flex-row items-center justify-between">
-          <div className="flex-1 lg:order-2">
-            <img
-              src={mainImg}
-              alt="Orange Chair"
-              className="mx-auto lg:mx-0"
-            />
-          </div>
-
-          <div className="flex-1 text-center lg:text-left lg:order-1">
-            <h1 className="text-5xl font-extrabold text-orange-500 leading-none">
-              New
-            </h1>
-            <h2 className="text-5xl font-extrabold text-orange-500 leading-none">
-              arrivals
-            </h2>
-            <p className="mt-4 text-gray-700">Timber Trove Furniture</p>
+              <h3 className="absolute top-52 right-80 text-6xl text-white font-MochiyPopOne rotate-90 font-semibold">2024</h3>
+              <p className="absolute -bottom-10 right-0 text-2xl rotate-90 font-semibold">2024  EDITION</p>
+            </div>
           </div>
         </div>
+        
 
-        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-          <img
-            src="https://via.placeholder.com/100x20" // Replace with actual image URL
-            alt="Branch"
-            className="mx-auto"
-          />
-        </div>
-      </div> */}
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
